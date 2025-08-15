1311c39642425d6df532c645e8e38eafecfe938e
Sarah Li <100035287+sli051357@users.noreply.github.com>
2025-08-15 10:32:46 -0700

    filter for due dates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // https://developers.notion.com/docs/create-a-notion-integration
 // https://github.com/makenotion/notion-sdk-js/blob/main/examples/intro-to-notion-api/intermediate/3-query-database.js
 // https://discord.com/developers/docs/quick-start/getting-started
+// https://discord.com/developers/docs/interactions/receiving-and-responding#followup-messages
 
 import { Client } from "@notionhq/client"
 import { config } from "dotenv"
@@ -14,24 +15,39 @@ const notion = new Client({
     auth: apiKey
 })
 
-async function findProperties(pageId) {
-    const response = await notion.blocks.children.list({
-        block_id: pageId
-    });
-    console.log(response)
-}
-
 async function queryDatabase(databaseId) {
     console.log("Querying database...")
-    const response = await notion.databases.query({
-        database_id: databaseId
-    })
+    const upcoming_due = await notion.databases.query({
+        database_id: databaseId,
+        filter: {
+            property: "Due Date",
+            date: {
+                next_week: {}
+            }
+        },
+    });
 
-    for (let i = 0; i < response.results.length; i++) {
-        console.log(response.results[i].id)
-        findProperties(response.results[i].id)
+    const upcoming_iteration = await notion.databases.query({
+        database_id: databaseId,
+        filter: {
+            property: "First Iteration Date",
+            date: {
+                next_week: {}
+            }
+        }
+    });
+
+    console.log("Due Date");
+    for (let i = 0; i < upcoming_due.results.length; i++) {
+        let pageData = upcoming_due.results[i].properties.Name.title;
+        console.log(pageData[0].plain_text);
     }
 
+    console.log("First iteration")
+    for (let i = 0; i < upcoming_iteration.results.length; i++) {
+        let pageData = upcoming_iteration.results[i].properties.Name.title;
+        console.log(pageData[0].plain_text)
+    }
 }
 
 async function main() {
